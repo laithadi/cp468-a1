ee688c25e59563aa002937d1b907784d45ae48ff
Jiachun Xiang <jcxiang1230@gmail.com>
2020-10-07 20:36:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    puzzle_size = int(input(
+        "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
+    while(puzzle_size != 8 and puzzle_size != 15 and puzzle_size != 24):
+        puzzle_size = int(input(
+            "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
+    print("You choose {}-puzzle, now it is  time to create the puzzle for you...".format(puzzle_size))
+    # create_puzzle(puzzle_size)
+    print("Puzzle created...Show puzzle for you now...")
+    # a_star_search()
+    # heuristic()
+
+
+if __name__ == "__main__":
+    main()

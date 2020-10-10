1408be84167bbf1de368fb6af76802a064820cb8
ramankala <rs.kala@hotmail.com>
2020-10-09 20:42:25 -0400

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -37,8 +37,15 @@ def initialState(size):
     """
     Returns the initial state of the puzzle 
     """
-    arr = np.arange(9).reshape(3,3)
-    np.random.shuffle(arr)
+    if size == 8:
+        arr = np.arange(9).reshape(3,3)
+        np.random.shuffle(arr)
+    elif size == 15:
+        arr = np.arange(16).reshape(4,4)
+        np.random.shuffle(arr)
+    else:
+        arr = np.arange(25).reshape(5,5)
+        np.random.shuffle(arr)
     return arr
 
 

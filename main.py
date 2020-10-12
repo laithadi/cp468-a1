28408c32a3658f6f3d67336c808560f15b68cacd
Jiachun Xiang <jcxiang1230@gmail.com>
2020-10-12 19:51:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def main():
         # if(not gameOver):
             h3_mt = h3(puzzle4Execute, goalState(puzzle_size))
         # check which heuristic function has least misplaced Tiles
-        if (h1 < h2 < h3):
-            smallest_mt = h1
-        elif (h2 < h1 < h3):
-            smallest_mt = h2
-        elif (h3 < h1 < h2):
-            smallest_mt = h3
+        if (h1_mt < h2_mt < h3_mt):
+            smallest_mt = h1_mt
+        elif (h2_mt < h1_mt < h3_mt):
+            smallest_mt = h2_mt
+        elif (h3_mt < h1_mt < h2_mt):
+            smallest_mt = h3_mt
             #     # create a OGpuzzle
             #     OGpuzzle = pz.initialState(puzzle_size)
 

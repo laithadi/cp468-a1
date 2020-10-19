f1bb969c4bb435e52b65e896dfd093ae19af510b
ramankala <rs.kala@hotmail.com>
2020-10-19 17:17:07 -0400

    fixed shuffled for input

--- puzzle.py
+++ puzzle.py
@@ -41,19 +41,25 @@ def initialState(size):
     """
     if size == 8:
         arr = np.arange(9).reshape(3, 3)
-        np.random.shuffle(arr)
         arr = np.where(arr == 0, None, arr)
+        arr = arr.ravel()
+        np.random.shuffle(arr)
+        arr = arr.reshape(3,3)
         arr = arr.tolist()
 
     elif size == 15:
         arr = np.arange(16).reshape(4, 4)
-        np.random.shuffle(arr)
         arr = np.where(arr == 0, None, arr)
+        arr = arr. ravel()
+        np.random.shuffle(arr)
+        arr = arr.reshape(4,4)
         arr = arr.tolist()
     else:
         arr = np.arange(25).reshape(5, 5)
-        np.random.shuffle(arr)
         arr = np.where(arr == 0, None, arr)
+        arr = arr.ravel()
+        np.random.shuffle(arr)
+        arr = arr.reshape(5,5)
         arr = arr.tolist()
     return arr
 

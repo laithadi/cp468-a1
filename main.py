8190ab99a5dbdb90c0cec37bb6bcb6ab43a20450
laithadi <laith_adi@hotmail.com>
2020-10-18 23:35:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,49 +3,32 @@ from puzzle import *
 
 
 def main():
-    puzzle_size = int(input(
-        "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
-    #originalPuzzle= pz()
-    # check if user input is 8/15/24, ask for input again if it is not
+
+    puzzle_size = int(input("What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
+
     while(puzzle_size != 8 and puzzle_size != 15 and puzzle_size != 24):
-        puzzle_size = int(input(
-            "What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
+        puzzle_size = int(input("What size of puzzle you want to have? Enter 8, 15 or 24 Please: "))
 
-    print("You choose {}-puzzle, now it is  time to create the puzzle for you...".format(puzzle_size))
-
-    print("Puzzle created...Show puzzle for you now...")
-    print("Show Table...")
+    print("Table: Puzzle Size {}".format(puzzle_size))
     print("----------------------------------------------------------------------------------------------------------------------------------")
     print("|            puzzle #            |                   num_steps                   |                   num_nodes                   |")
-    # heuristic()
+   
     j = 0
-    # # execute 100 different random puzzles 47
+
     while (j != 100):
-
-        originalPuzzle = pz.initialState(puzzle_size)
-        goalPuzzle = pz.goalState(puzzle_size)
-
-    #     # heuristic 1
-        h1_num_steps, h1_num_nodes = pz.aStarh1(
-            originalPuzzle, goalPuzzle, puzzle_size)
-
-    #     # heursitic 2
-        h2_num_steps, h2_num_nodes = pz.aStarh2(
-            originalPuzzle, goalPuzzle, puzzle_size)
-
-    #     # heuristic 3
-        h3_num_steps, h3_num_nodes = pz.aStarh3(
-            originalPuzzle, goalPuzzle, puzzle_size)
-
-        j += 1
-
+        # create the initial puzzle and goal puzzle 
+        originalPuzzle = pz.initialState(puzzle_size)  
+        goalPuzzle = pz.goalState(puzzle_size)  
+        # run the astar algo using the 3 different heurisitics 
+        h1_num_steps, h1_num_nodes = 0, 0  
+        h2_num_steps, h2_num_nodes = pz.aStarh2(originalPuzzle, goalPuzzle, puzzle_size)
+        h3_num_steps, h3_num_nodes = 0, 0 
+         # increment j to keep track of the number of puzzles being solved
+        j += 1 
+        # print the results 
         print("                {}                         h1 = {}, h2 = {}, h3 = {}                      h1 = {}, h2 = {}, h3 = {}           ".format(
             j, h1_num_steps, h2_num_steps, h3_num_steps, h1_num_nodes, h2_num_nodes, h3_num_nodes))
         print("----------------------------------------------------------------------------------------------------------------------------------")
-
         
-        
-
-
 if __name__ == "__main__":
     main()
